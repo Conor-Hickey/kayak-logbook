620f9cceedeebead6861c9503d986a7c3c1f6745
Conor <conor@minviro.com>
2024-03-21 16:42:12 +0000

    toggle dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    
-      <body className={inter.className}><Providers>{children}</Providers></body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers>{children}</Providers>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
-"use client"
+"use client";
 import { useSession } from "next-auth/react";
 import { AuthContent } from "@/content/landing-page/Auth";
 import { createClient } from "@supabase/supabase-js";
 import KayakTrips from "@/components/kayak-trip-lists";
+import { ModeToggle } from "@/components/dark-mode-toggle";
 
-const supabase = createClient("https://mtuvumtbujjuwtuxhzvn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10dXZ1bXRidWpqdXd0dXhoenZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwMjIxMzYsImV4cCI6MjAyNjU5ODEzNn0.ofAEbhfn6lHJnFwp92VUBm-HzrTyMV3-nwtphCa1wuY");
+const supabase = createClient(
+  "https://mtuvumtbujjuwtuxhzvn.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10dXZ1bXRidWpqdXd0dXhoenZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwMjIxMzYsImV4cCI6MjAyNjU5ODEzNn0.ofAEbhfn6lHJnFwp92VUBm-HzrTyMV3-nwtphCa1wuY"
+);
 
 export default function App() {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	return session ? (
-	<KayakTrips/>
-	) : (
-		<AuthContent />
-	);
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <ModeToggle />
+      </div>
+      {session ? <KayakTrips /> : <AuthContent />}
+    </>
+  );
 }
