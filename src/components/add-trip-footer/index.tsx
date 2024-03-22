aa1a4bbc1e1648a14591d9864d8af53f860cf213
Conor <conor@minviro.com>
2024-03-22 10:38:01 +0000

    use env

--- src/components/add-trip-footer/index.tsx
+++ src/components/add-trip-footer/index.tsx
@@ -7,10 +7,8 @@ import { toast } from "../ui/use-toast";
 export const AddTripFooter = () => {
 	const addTripHandler = async () => {
 		try {
-			const supabaseUrl = "https://mtuvumtbujjuwtuxhzvn.supabase.co";
-			const supabaseAnonKey =
-				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10dXZ1bXRidWpqdXd0dXhoenZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwMjIxMzYsImV4cCI6MjAyNjU5ODEzNn0.ofAEbhfn6lHJnFwp92VUBm-HzrTyMV3-nwtphCa1wuY";
-
+			const supabaseUrl = process.env.REACT_APP_SUPABASE_URLSUPABASE_URL!;
+			const supabaseAnonKey = process.env.REACT_APP_SUPABASE_URLSUPABASE_KEY!;
 			const supabase = createClient(supabaseUrl, supabaseAnonKey);
 			const { error } = await supabase
 				.from("kayakingtrips")
--- src/components/kayak-trips/index.tsx
+++ src/components/kayak-trips/index.tsx
@@ -4,10 +4,8 @@ import { KayakTrip, columns } from "./columns";
 
 import { DataTable } from "./data-table";
 
-const supabaseUrl = "https://mtuvumtbujjuwtuxhzvn.supabase.co";
-const supabaseAnonKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10dXZ1bXRidWpqdXd0dXhoenZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwMjIxMzYsImV4cCI6MjAyNjU5ODEzNn0.ofAEbhfn6lHJnFwp92VUBm-HzrTyMV3-nwtphCa1wuY";
-
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URLSUPABASE_URL!;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_URLSUPABASE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function KayakTrips() {
